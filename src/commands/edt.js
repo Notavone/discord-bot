cfa4d04173baded443cd503d4ebac67c1d8ee6a4
Notavone <notavone@gmail.com>
2021-01-04 22:55:31 +0100

    rollback skip, add edt

---
+++ src/commands/edt.js
@@ -0,0 +1,44 @@
+const { Group } = require('../utils/edt.js')
+
+module.exports = {
+  name: 'edt',
+  aliases: ['iut'],
+  help: "Permet de récupérer l'emploi du temps d'un groupe en fonction de la semaine",
+  syntax: 'edt <group> [<week>]',
+  run: async (client, message, args) => {
+    const groupName = args[0].toLowerCase()
+    const week = Number(args[1])
+    const groups = [
+      new Group('s1-a1', 4673),
+      new Group('s1-a2', 4674),
+      new Group('s1-b1', 4676),
+      new Group('s1-b2', 4675),
+      new Group('s1-c1', 4677),
+      new Group('s1-c2', 4678),
+      new Group('s1-d1', 4679),
+      new Group('s2-a1', 4691),
+      new Group('s2-a2', 4692),
+      new Group('s2-b1', 4685),
+      new Group('s2-b2', 4686),
+      new Group('s2-c1', 4688),
+      new Group('s2-c2', 4689),
+      new Group('s2-d1', 4694),
+      new Group('s3-a1', 4707),
+      new Group('s3-a2', 4708),
+      new Group('s3-b1', 4701),
+      new Group('s3-b2', 4702),
+      new Group('s3-c1', 4704),
+      new Group('s3-c2', 4705),
+      new Group('s4-a1', 10874),
+      new Group('s4-a2', 10875),
+      new Group('s4-b1', 10876),
+      new Group('s4-b2', 10877),
+      new Group('s4-c1', 16238),
+      new Group('s4-c2', 19973)
+    ]
+    const group = groups.find((grp) => grp.getName() === groupName)
+
+    if (!group) return message.reply('Erreur')
+    await group.displayEDT(message, week)
+  }
+}
--- src/commands/skip.js
+++ src/commands/skip.js
@@ -12,9 +12,6 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setDescription('Il n\'y a pas de file d\'attente pour ce serveur')
     if (!queue) return message.channel.send(embed)
-    if (Number(nb) > 2) for (let i = 0; i < nb - 1; i++) {
-      await queue.skip(false)
-    }
     await queue.skip(true)
   }
 }
