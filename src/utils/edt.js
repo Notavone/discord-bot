6dc9de967f2cbb507a91f9d3211b6841bdee64d3
Notavone <notavone@gmail.com>
2021-01-04 23:31:04 +0100

    week calibration

--- src/utils/edt.js
+++ src/utils/edt.js
@@ -21,12 +21,13 @@ class Group {
   }
 
   async displayEDT (message, week) {
-    week = Math.floor(isNaN(week) ? 1 : Number(week) > 5 ? 5 : Number(week) < 1 ? 1 : Number(week))
+    week = Math.floor(isNaN(week) ? 1 : Number(week) >= 5 ? 6 : Number(week) < 1 ? 1 : Number(week) + 1)
     await request(`https://sedna.univ-fcomte.fr/jsp/custom/ufc/mplanif.jsp?id=${this.id.toString()}&jours=${(7 * week).toString()}`, async (err, res, body) => {
       if (err) throw err
       const url = body.match(/<a href="(.*)">Affichage planning<\/a>/)[1].replace(/&width=[0-9]*&height=[0-9]*&/, '&width=1080&height=720&')
       const embed = new Discord.MessageEmbed()
-        .setDescription(`EDT du groupe ${this.name.toUpperCase()}`)
+        .setTitle(`Emploi du temps du groupe ${this.name.toUpperCase()}`)
+        .setDescription(`${week === 1 ? 'semaine actuelle' : `+${week - 1} semaine(s)`}`)
         .setImage(url)
       await message.channel.send(embed)
     })
