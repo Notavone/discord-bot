970df0ebfd88271089d77265b0df1455ee620e89
Notavone <notavone@gmail.com>
2021-01-11 17:45:46 +0100

    add more groups, remove saturday

--- src/utils/edt.js
+++ src/utils/edt.js
@@ -24,7 +24,9 @@ class Group {
     week = Math.floor(isNaN(week) ? 1 : Number(week) >= 5 ? 6 : Number(week) < 1 ? 1 : Number(week) + 1)
     await request(`https://sedna.univ-fcomte.fr/jsp/custom/ufc/mplanif.jsp?id=${this.id.toString()}&jours=${(7 * week).toString()}`, async (err, res, body) => {
       if (err) throw err
-      const url = body.match(/<a href="(.*)">Affichage planning<\/a>/)[1].replace(/&width=[0-9]*&height=[0-9]*&/, '&width=1080&height=720&')
+      const url = body.match(/<a href="(.*)">Affichage planning<\/a>/)[1]
+        .replace(/&width=[0-9]*&height=[0-9]*&/, '&width=1080&height=720&')
+        .replace('&idPianoDay=0%2C1%2C2%2C3%2C4%2C5', '&idPianoDay=0%2C1%2C2%2C3%2C4')
       const embed = new Discord.MessageEmbed()
         .setTitle(`Emploi du temps du groupe ${this.name.toUpperCase()}`)
         .setDescription(`${week === 1 ? 'semaine actuelle' : `+${week - 1} semaine(s)`}`)
